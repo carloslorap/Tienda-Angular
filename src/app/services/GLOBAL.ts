d2e6b6ef977745a43eb519b155eaa68f4216c1f6
carloslorap <74977007@certus.edu.pe>
2024-03-29 12:30:38 -0500

    ahora si

--- src/app/services/GLOBAL.ts
+++ src/app/services/GLOBAL.ts
@@ -1,4 +1,5 @@
 export var GLOBAL ={
-  url:'http://localhost:4000/api/'
+  url:'https://servereccomersangular.onrender.com/api/'
 }
 //url:'https://servereccomersangular.onrender.com/api/'
+//url:'http://localhost:4000/api/'
