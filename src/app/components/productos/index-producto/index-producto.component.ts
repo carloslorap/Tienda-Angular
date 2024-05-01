dcab99b373e8f52bc041d06f3bc74dea1e873488
carloslorap <74977007@certus.edu.pe>
2024-05-01 15:46:27 -0500

    cambio miercoles dia del trabajador!!!

--- src/app/components/productos/index-producto/index-producto.component.ts
+++ src/app/components/productos/index-producto/index-producto.component.ts
@@ -2,24 +2,23 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { GLOBAL } from 'src/app/services/GLOBAL';
 import { ClienteService } from 'src/app/services/cliente.service';
-import { io } from "socket.io-client";
+import { io } from 'socket.io-client';
 import { GuestService } from 'src/app/services/guest.service';
 declare var noUiSlider: any;
 declare var $: any;
-declare var iziToast:any;
+declare var iziToast: any;
 
 @Component({
   selector: 'app-index-producto',
   templateUrl: './index-producto.component.html',
   styleUrls: ['./index-producto.component.css'],
 })
-
 export class IndexProductoComponent implements OnInit {
-  public token :any = localStorage.getItem('token');
+  public token: any = localStorage.getItem('token');
   public config_global: any = {};
 
-
   public filter_categoria = '';
+  public wishlistIds: { [productId: string]: boolean } = {};
   public productos: Array<any> = [];
   public filtro_producto = '';
   public url: any;
@@ -28,15 +27,15 @@ export class IndexProductoComponent implements OnInit {
   public pageSize = 20;
   public page = 1;
   public sort_by = 'Defecto';
-  public carrito_data : any ={
+  public carrito_data: any = {
     variedad: '',
-    cantidad: 1
-  }
+    cantidad: 1,
+  };
   public user_lc: any = {};
-  public wishlist_data : any ={}
+  public wishlist_data: any = {};
   public socket = io('https://servereccomersangular.onrender.com');
-  public btn_cart = false
-  public descuento_activo :any = undefined
+  public btn_cart = false;
+  public descuento_activo: any = undefined;
   public load_data = true;
 
   constructor(
@@ -45,6 +44,12 @@ export class IndexProductoComponent implements OnInit {
     private _route: ActivatedRoute
   ) {
     this.url = GLOBAL.url;
+
+    // Restaurar el estado local de la lista de deseos desde localStorage al iniciar
+    const wishlistIdsString = localStorage.getItem('wishlistIds');
+    if (wishlistIdsString) {
+      this.wishlistIds = JSON.parse(wishlistIdsString);
+    }
 
     this._clienteService.obtener_config_publico().subscribe((response) => {
       this.config_global = response.data;
@@ -95,31 +100,25 @@ export class IndexProductoComponent implements OnInit {
         $('.cs-range-slider-value-min').val(values[0]);
         $('.cs-range-slider-value-max').val(values[1]);
       });
-    },300)
+    }, 300);
     $('.noUi-tooltip').css('font-size', '11px');
 
-    this._guestService.obtener_descuento_activo().subscribe(response=>{
-
+    this._guestService.obtener_descuento_activo().subscribe((response) => {
       if (response.data != undefined) {
-        this.descuento_activo = response.data[0]
+        this.descuento_activo = response.data[0];
         console.log(this.descuento_activo);
-
-
-      }else{
+      } else {
         this.descuento_activo = undefined;
 
         console.log(this.descuento_activo);
-
       }
-
-
-    })
-
-
+    });
   }
 
-  obtener_carrito(){
-    this._clienteService.obtener_carrito_cliente(this.user_lc._id,this.token).subscribe(response=>{})
+  obtener_carrito() {
+    this._clienteService
+      .obtener_carrito_cliente(this.user_lc._id, this.token)
+      .subscribe((response) => {});
   }
 
   buscar_categorias() {
@@ -197,174 +196,176 @@ export class IndexProductoComponent implements OnInit {
           this.productos = response.data;
           this.load_data = false;
         });
-    }else if(this.sort_by == 'Popularidad'){
-      this.productos.sort(function (a, b){
+    } else if (this.sort_by == 'Popularidad') {
+      this.productos.sort(function (a, b) {
         if (a.nventas < b.nventas) {
-            return 1
+          return 1;
         }
         if (a.nventas > b.nventas) {
-          return -1
+          return -1;
         }
-        return 0
-      })
-    }else if(this.sort_by == '+-Precio'){
-      this.productos.sort(function (a, b){
+        return 0;
+      });
+    } else if (this.sort_by == '+-Precio') {
+      this.productos.sort(function (a, b) {
         if (a.precio < b.precio) {
-            return 1
+          return 1;
         }
         if (a.precio > b.precio) {
-          return -1
+          return -1;
         }
-        return 0
-      })
-    }else if(this.sort_by == '-+Precio'){
-      this.productos.sort(function (a, b){
+        return 0;
+      });
+    } else if (this.sort_by == '-+Precio') {
+      this.productos.sort(function (a, b) {
         if (a.precio > b.precio) {
-            return 1
+          return 1;
         }
         if (a.precio < b.precio) {
-          return -1
+          return -1;
         }
-        return 0
-      })
-    }else if(this.sort_by == 'azTitulo'){
-      this.productos.sort(function (a, b){
+        return 0;
+      });
+    } else if (this.sort_by == 'azTitulo') {
+      this.productos.sort(function (a, b) {
         if (a.titulo > b.titulo) {
-            return 1
+          return 1;
         }
         if (a.titulo < b.titulo) {
-          return -1
+          return -1;
         }
-        return 0
-      })
-    }else if(this.sort_by == 'zaTitulo'){
-      this.productos.sort(function (a, b){
+        return 0;
+      });
+    } else if (this.sort_by == 'zaTitulo') {
+      this.productos.sort(function (a, b) {
         if (a.titulo < b.titulo) {
-            return 1
+          return 1;
         }
         if (a.titulo > b.titulo) {
-          return -1
+          return -1;
         }
-        return 0
-      })
+        return 0;
+      });
     }
   }
 
-
-
-
-  agregar_producto(producto:any){
-      if (this.carrito_data.cantidad <= producto.stock) {
-        let data = {
-          producto: producto._id,
-          cliente:localStorage.getItem('_id'),
-          cantidad: 1,
-          variedad: producto.variedades[0].titulo
-        }
-        this.btn_cart = true
-        this._clienteService.agregar_carrito_cliente(data,this.token).subscribe(response=>{
-          if (response.data == undefined) {
-            iziToast.show({
-              title:'ERROR',
-              titleColor: '#FF0000',
-              class:'text-danger',
-              position:'topRight',
-              message:"El producto ya existe en el carrito"
-             })
-             this.btn_cart = false
-          }else{
-            iziToast.show({
-              title:'SUCCESS',
-              titleColor: '#05B481',
-              class:'text-danger',
-              position:'topRight',
-              message:'Se agrego el produto al carrito'
-             })
-            this.btn_cart = false
-            producto.isInWishlist = false;
-            this.socket.emit('add-carrito',{data:true})
-          }
-        })
-      }else{
-        iziToast.show({
-          title:'ERROR',
-          titleColor: '#FF0000',
-          class:'text-danger',
-          position:'topRight',
-          message:"El maximo disponible es:" + producto.stock
-         })
-      }
-
-  }
-
-
-  agregar_wishlist(producto:any){
-
+  agregar_producto(producto: any) {
+    if (this.carrito_data.cantidad <= producto.stock) {
       let data = {
         producto: producto._id,
-        cliente:localStorage.getItem('_id'),
-
-      }
-      this.btn_cart = true
-      this._clienteService.agregar_wishlist_cliente(data,this.token).subscribe(response=>{
-        if (response.data == undefined) {
-          iziToast.show({
-            title:'ERROR',
-            titleColor: '#FF0000',
-            class:'text-danger',
-            position:'topRight',
-            message:"El producto ya existe en la lista de deseo"
-           })
-           this.btn_cart = false
-        }else{
-          producto.isInWishlist = true;
-
-          iziToast.show({
-            title:'SUCCESS',
-            titleColor: '#05B481',
-            class:'text-danger',
-            position:'topRight',
-            message:'Se agrego el produto a su lista'
-           })
-          this.btn_cart = false
-
-          // this.socket.emit('add-carrito',{data:true})
-        }
-      })
-
-
-}
-
-eliminarWishlist(producto: any) {
-  let data = {
-    producto: producto._id,
-    cliente: localStorage.getItem('_id'),
-  };
-
-  this._clienteService.eliminar_wishlist_cliente(data, this.token).subscribe(response => {
-    if (response.data != undefined) {
-      producto.isInWishlist = false;
-
-      iziToast.show({
-        title: 'SUCCESS',
-        titleColor: '#05B481',
-        class: 'text-danger',
-        position: 'topRight',
-        message: 'Se eliminó el producto de la lista de deseos'
-      });
+        cliente: localStorage.getItem('_id'),
+        cantidad: 1,
+        variedad: producto.variedades[0].titulo,
+      };
+      this.btn_cart = true;
+      this._clienteService
+        .agregar_carrito_cliente(data, this.token)
+        .subscribe((response) => {
+          if (response.data == undefined) {
+            iziToast.show({
+              title: 'ERROR',
+              titleColor: '#FF0000',
+              class: 'text-danger',
+              position: 'topRight',
+              message: 'El producto ya existe en el carrito',
+            });
+            this.btn_cart = false;
+          } else {
+            iziToast.show({
+              title: 'SUCCESS',
+              titleColor: '#05B481',
+              class: 'text-danger',
+              position: 'topRight',
+              message: 'Se agrego el produto al carrito',
+            });
+            this.btn_cart = false;
+            delete this.wishlistIds[producto._id];
+            localStorage.setItem('wishlistIds', JSON.stringify(this.wishlistIds));
+            this.socket.emit('add-carrito', { data: true });
+          }
+        });
     } else {
       iziToast.show({
         title: 'ERROR',
         titleColor: '#FF0000',
         class: 'text-danger',
         position: 'topRight',
-        message: 'Error al eliminar el producto de la lista de deseos'
+        message: 'El maximo disponible es:' + producto.stock,
       });
     }
-  });
-}
+  }
 
+  agregar_wishlist(producto: any) {
+    let data = {
+      producto: producto._id,
+      cliente: localStorage.getItem('_id'),
+    };
+    this.btn_cart = true;
+    this._clienteService
+      .agregar_wishlist_cliente(data, this.token)
+      .subscribe((response) => {
+        if (response.data == undefined) {
+          iziToast.show({
+            title: 'ERROR',
+            titleColor: '#FF0000',
+            class: 'text-danger',
+            position: 'topRight',
+            message: 'El producto ya existe en la lista de deseo',
+          });
+          this.btn_cart = false;
+        } else {
+          // this.user_new_wishList.isInWishlist = true;
 
+          iziToast.show({
+            title: 'SUCCESS',
+            titleColor: '#05B481',
+            class: 'text-danger',
+            position: 'topRight',
+            message: 'Se agrego el produto a su lista',
+          });
+          this.btn_cart = false;
+          // Actualizar el estado local y guardar en localStorage
+          this.wishlistIds[producto._id] = true;
+          localStorage.setItem('wishlistIds', JSON.stringify(this.wishlistIds));
 
+          // this.socket.emit('add-carrito',{data:true})
+        }
+      });
+  }
 
+  eliminarWishlist(producto: any) {
+    let data = {
+      producto: producto._id,
+      cliente: localStorage.getItem('_id'),
+    };
+
+    this._clienteService
+      .eliminar_wishlist_cliente(data, this.token)
+      .subscribe((response) => {
+        if (response.data != undefined) {
+          // Actualizar el estado local y guardar en localStorage
+          delete this.wishlistIds[producto._id];
+          localStorage.setItem('wishlistIds', JSON.stringify(this.wishlistIds));
+          iziToast.show({
+            title: 'SUCCESS',
+            titleColor: '#05B481',
+            class: 'text-danger',
+            position: 'topRight',
+            message: 'Se eliminó el producto de la lista de deseos',
+          });
+        } else {
+          iziToast.show({
+            title: 'ERROR',
+            titleColor: '#FF0000',
+            class: 'text-danger',
+            position: 'topRight',
+            message: 'Error al eliminar el producto de la lista de deseos',
+          });
+        }
+      });
+  }
+
+  isWishlist(productId: string): boolean {
+    return this.wishlistIds[productId] || false;
+  }
 }
